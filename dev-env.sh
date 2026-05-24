0229b4f7cdcfb42591c69806379cbe23faad8f15
Damian <mrugala.damian@gmail.pl>
2026-05-24 14:39:37 +0200

    chore: improve git helper aliases

--- dev-env.sh
+++ dev-env.sh
@@ -1,8 +1,8 @@
 alias up="./vendor/bin/sail up -d"
 alias down="./vendor/bin/sail down"
 alias build="./vendor/bin/sail npm run build"
-alias l="git log --oneline"
-alias log=l
+alias l="git --no-pager log --oneline -6"
+alias log="git --no-pager log --oneline"
 alias b="git branch"
 alias branch=b
 alias pull="git pull"
@@ -20,6 +20,8 @@ alias c="git commit -m"
 alias commit=c
 alias m="git merge"
 alias merge=m
+alias t="git tag --sort=v:refname | tail -6"
+alias tag="git --no-pager tag"
 alias backup="./codeplatform/tools/bin/backup"
 alias backup-list="./codeplatform/tools/bin/backup-list"
 alias backup-last="./codeplatform/tools/bin/backup-last"
