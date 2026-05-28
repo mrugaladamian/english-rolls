631c4192b94f0472f987e67704cb4d1cd91c0e5b
Damian <mrugala.damian@gmail.pl>
2026-05-28 14:56:15 +0200

    chore: update branding icons and logo display

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'English Rolls')</title>
     <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
+    <link rel="apple-touch-icon" href="{{ asset('apple-touch-icon.png') }}">
     @vite(config('vite.entries.app'))
 </head>
 
