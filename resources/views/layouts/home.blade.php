aa3c64eba5405de1d66b862adee78fbb4592f07d
Damian <mrugala.damian@gmail.pl>
2026-05-28 13:01:03 +0200

    feat: add site favicon

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'English Rolls')</title>
+    <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
     @vite(config('vite.entries.app'))
 </head>
 
--- resources/views/layouts/rolls.blade.php
+++ resources/views/layouts/rolls.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'English Rolls')</title>
+    <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
     <script src="https://code.responsivevoice.org/responsivevoice.js?key=vFVsh7I0"></script>
     @vite(config('vite.entries.app'))
     <script src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js" defer></script>
